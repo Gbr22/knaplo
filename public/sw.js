06cb0dd1a57f29505a6e0312dce1a20f0cee3138
Gábor Kriskó <gabor.krisko@gmail.com>
2020-03-06 19:31:40 +0100

    don't cache kreta data in sw

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,13 @@
 const cache_name = "cache-v1";
 
-let cached_urls = [
+let cached_urls = [ //precache
     '/',
     '/?source=pwa&utm_source=pwa',
     "/api/institute"
 ]
+let no_cache = [
+    "/api/data"
+];
 
 function preCache() {
     return caches.open(cache_name).then(function (cache) {
@@ -17,18 +20,38 @@ self.addEventListener('install', function(e) {
     e.waitUntil(preCache());
 });
 
+function checkCache(u){
+    
+    for (let url of no_cache){
+        if (u.indexOf(url) != -1){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function fetchResource(req){
     return new Promise(function(resolve,reject){
-        let time = req.url.indexOf("/api") == -1 ? 400 : 10000;
-        let timeout = setTimeout(function(){
-            reject("Timed out");
-        },time);
+        let timeout = -1;
+        
+
+        if (checkCache(req.url)){
+            timeout = setTimeout(function(){
+                reject("Timed out");
+            },400);
+        }
+        
         fetch(req).then(function(response) {
-            return caches.open(cache_name).then(function(cache) {
-              cache.put(req, response.clone());
-              resolve(response);
-              clearTimeout(timeout);
-            });  
+            if (checkCache(req.url)){
+                return caches.open(cache_name).then(function(cache) {
+                    cache.put(req, response.clone());
+                    resolve(response);
+                    clearTimeout(timeout);
+                });
+            } else {
+                resolve(response);
+            }
         });
         
     })
