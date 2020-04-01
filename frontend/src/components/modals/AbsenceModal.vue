5a7a37d4b908c98107b004be5e7a29dc722a1f72
Gábor Kriskó <gabor.krisko@gmail.com>
2020-04-01 21:12:07 +0200

    only show header in absence

--- frontend/src/components/modals/AbsenceModal.vue
+++ frontend/src/components/modals/AbsenceModal.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-      <h3>{{ (new Date(obj.date)).toLocaleDateString() }}</h3>
-      <TimelineItem v-for="item in obj.absences" :item="item" v-bind:key="item.key" />
+      <h3>{{ obj.header }}</h3>
+      <TimelineItem v-for="item in absences" :item="item" v-bind:key="item.key" />
   </div>
 </template>
 
@@ -11,6 +11,16 @@ import TimelineItem from '../TimelineItem';
 export default {
     name:"AbsenceModal",
     props:["obj"],
+    data(){
+        let absences = JSON.parse(JSON.stringify(this.obj.absences)).map((e)=>{
+            e.header = e.desc;
+            e.desc = "";
+            return e;
+        })
+        return {
+            absences
+        }
+    },
     components:{
         TimelineItem
     }
@@ -21,5 +31,6 @@ export default {
     h3 {
         text-align: center;
         margin-bottom: 8px;
+        padding: 0 20px;
     }
 </style>
