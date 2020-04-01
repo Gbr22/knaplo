1baacc2754eac7ae3058682e9eede907d3be9287
Gábor Kriskó <gabor.krisko@gmail.com>
2020-04-01 21:30:40 +0200

    back button and esc to exit modal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,8 @@ window.GlobalState = GlobalState;
 
 import VueVirtualScroller from 'vue-virtual-scroller'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+import { closeModal } from './components/Modal';
+
 
 Vue.use(VueVirtualScroller)
 
@@ -16,3 +18,17 @@ new Vue({
   data:GlobalState,
   render: h => h(App),
 }).$mount('#app')
+
+
+function onBackAction(){
+  closeModal();
+}
+window.onpopstate = function(){
+  window.history.pushState({}, '');
+  onBackAction();
+}
+window.onkeydown = function(event){
+  if (event.keyCode == 27){
+    onBackAction();
+  }
+}
