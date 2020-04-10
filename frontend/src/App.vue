46a5f05c927db1cc3caf70922fcb68fda613e0c0
Gábor Kriskó <gabor.krisko@gmail.com>
2020-04-10 11:13:22 +0200

    code splitting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,7 @@
   <div id="app">
     <Login v-if="!loggedIn" />
     <div v-if="loggedIn" id="pageviewer">
-      <div id="pageview_inner">
-        <Averages v-if="currentMenu == 'avgs'" />
-        <Timeline v-show="currentMenu == 'timeline'"/>
-        <Timetable v-show="currentMenu == 'timetable'"/>
-        <Homework v-show="currentMenu == 'homework'"/>
-        <Settings v-if="currentMenu == 'settings'"/>
-        <HalfYr v-if="currentMenu == 'halfyr'"/>
-      </div>
+      <Screens />
       <Modal />
     </div>
     <Nav v-if="loggedIn" />
@@ -19,32 +12,19 @@
 
 <script>
 import Login from './view/Login';
-import Settings from './view/Settings';
-import HalfYr from './view/HalfYr';
 import GlobalState from './globalState';
 import MessageDisplay from './components/MessageDisplay';
 import Nav from './components/Nav';
-import Timeline from './view/Timeline';
 import Modal from './components/Modal';
-import Averages from './view/Averages';
-import Timetable from './view/Timetable.vue';
-import Homework from './view/Homework.vue';
-
-
 
 export default {
   name: 'App',
   components: {
-    Login,
-    Settings,
     MessageDisplay,
     Nav,
-    HalfYr,
-    Timeline,
     Modal,
-    Averages,
-    Timetable,
-    Homework
+    Login,
+    Screens:()=>import("./Screens"),
   },
   data:()=>{
     return GlobalState
---
+++ frontend/src/Screens.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="pageview_inner">
+        <Averages v-if="currentMenu == 'avgs'" />
+        <Timeline v-show="currentMenu == 'timeline'"/>
+        <Timetable v-show="currentMenu == 'timetable'"/>
+        <Homework v-show="currentMenu == 'homework'"/>
+        <Settings v-if="currentMenu == 'settings'"/>
+        <HalfYr v-if="currentMenu == 'halfyr'"/>
+    </div>
+</template>
+
+<script>
+import Settings from './view/Settings';
+import HalfYr from './view/HalfYr';
+import Timeline from './view/Timeline';
+import Averages from './view/Averages';
+import Timetable from './view/Timetable.vue';
+import Homework from './view/Homework.vue';
+
+export default {
+    name:"Screens",
+    components:{
+        Timetable,
+        Homework,
+        Settings,
+        HalfYr,
+        Timeline,
+        Averages,
+    },
+    data:()=>{
+        return GlobalState
+    }
+}
+</script>
+
+<style>
+
+</style>
