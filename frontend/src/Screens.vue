11959428713afe54c850a9307ddee39f022898de
Gábor Kriskó <gabor.krisko@gmail.com>
2020-04-18 15:25:01 +0200

    add spinner

--- frontend/src/Screens.vue
+++ frontend/src/Screens.vue
@@ -1,5 +1,15 @@
 <template>
-    <div id="pageview_inner">
+    <div id="pageview_inner" ref="screens" class="container pull-to-refresh-material">
+        <div class="ptr pull-to-refresh-material__control">
+            <svg class="icon pull-to-refresh-material__icon" fill="#4285f4" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+                <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+
+            <svg class="pull-to-refresh-material__spinner" width="24" height="24" viewBox="25 25 50 50">
+                <circle class="pull-to-refresh-material__path" cx="50" cy="50" r="20" fill="none" stroke="#4285f4" stroke-width="4" stroke-miterlimit="10" />
+            </svg>
+        </div>
         <Averages v-if="currentMenu == 'avgs'" />
         <Timeline v-show="currentMenu == 'timeline'"/>
         <Timetable v-show="currentMenu == 'timetable'"/>
@@ -17,6 +27,10 @@ import Averages from './view/Averages';
 import Timetable from './view/Timetable.vue';
 import Homework from './view/Homework.vue';
 
+import pullToRefresh from 'mobile-pull-to-refresh';
+import ptrAnimatesMaterial from 'mobile-pull-to-refresh/dist/styles/material/animates';
+import 'mobile-pull-to-refresh/dist/styles/material/style.css';
+
 export default {
     name:"Screens",
     components:{
@@ -29,11 +43,32 @@ export default {
     },
     data:()=>{
         return GlobalState
+    },
+    mounted(){
+        console.log("ref",this.$refs.screens);
+        const destroyCb = pullToRefresh({
+        container: this.$refs.screens,
+        animates: ptrAnimatesMaterial,
+        
+        refresh() {
+            return new Promise(resolve => {
+                
+                setTimeout(resolve, 2000);
+            })
+        }
+        })
     }
 }
 </script>
 
 <style>
+.ptr {
+    background-color: var(--element-color);
+    position: fixed;
+}
+.ptr .icon {
+    stroke: none;
+}
 .link, #credits a {
     color: var(--link-color);
     text-decoration: none;
