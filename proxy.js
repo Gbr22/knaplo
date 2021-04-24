acdaeb41d3fd99c2133523a92a47afae73eb8705
Gábor Kriskó <gabor.krisko@gmail.com>
2021-04-24 22:21:23 +0200

    remove proxy agents

--- proxy.js
+++ proxy.js
@@ -1,46 +1,4 @@
-
-const { URL } = require('url');
-
-const { http, https } = require('follow-redirects');
-// Create an HTTP tunneling proxy
-
-const { spawn } = require("child_process");
-let Path = require("path");
-
-/* let torConfPath = Path.join(__dirname,"torrc");
-console.log("tor conf path",torConfPath);
-const tor = spawn("tor", ["-f",torConfPath]); */
-
 const fetch = require('node-fetch');
-const fs = require("fs");
-var SocksProxyAgent = require('socks-proxy-agent');
- 
-/* var agent = new SocksProxyAgent("socks://localhost:9050"); */
-var args = process.argv.slice(2);
-let dev = args.includes("--dev");
-
-
-
-let agents = fs.readFileSync("proxylist.csv").toString()
-    .split("\n").filter(e=>e).map(e=>e.replace(/\r/g,""))
-    .map(e=>e.split(",")).map(e=>{
-        let [ip, port, protocol] = e;
-        let protocols = {
-            "SOCKS4":"socks4"
-        };
-        let p = protocols[protocol];
-        return new SocksProxyAgent(`${p}://${ip}:${port}`)
-    });
-
-
-function wait(ms){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve();
-        },ms);
-    })
-}
-
 module.exports = (req,res)=>{
     
     
@@ -57,39 +15,16 @@ module.exports = (req,res)=>{
         }
     }
     
-    let promises = [];
-    let controllers = [];
-
-    for (let i = 0; i < agents.length; i++) {
-        const agent = agents[i];
-
-        const controller = new AbortController();
-        const signal = controller.signal;
-        controllers.push(controller);
-
-        let options = {
-            headers,
-            method:req.method,
-            agent:agent,
-            redirect:redirect,
-            signal,
-        }
-        if (req.method == "POST"){
-            options.body = req.body;
-        }
-        
-        let promise = fetch(url,options);
-        promises.push(promise);
+    let options = {
+        headers,
+        method:req.method,
+        redirect:redirect,
+    }
+    if (req.method == "POST"){
+        options.body = req.body;
     }
     
-    wait(5000).then(()=>{
-        controllers.forEach(e=>{
-            e.abort();
-        })
-        res.status(500);
-        res.send();
-    })
-    Promise.any(promises).then(r=>{
+    fetch(url,options).then(r=>{
         let status = r.status;
         if (status >= 300 && status < 400){
             status = 299;
@@ -109,9 +44,6 @@ module.exports = (req,res)=>{
 
         res.set(h);
         r.body.pipe(res);
-        controllers.forEach(e=>{
-            e.abort();
-        })
     }).catch(err=>{
         res.status(500);
         res.send();
